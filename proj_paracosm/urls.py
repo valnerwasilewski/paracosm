578ce73113b8410904e876afa6bab82e176d0b8f
Valner Wasilewski <valnerwasilewski@gmail.com>
2025-06-14 22:57:54 -0300

    :package: Updated and correct according to isort and flake8

--- proj_paracosm/urls.py
+++ proj_paracosm/urls.py
@@ -14,21 +14,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
 
-from app_signup.views import home, signup_view, signin_view, userslist_view, register_user
-
-
+from app_signup.views import (home, register_user, registration_completed_view,
+                              signin_view, signup_view, userslist_view)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'), #página inicial
+    path('', home, name='home'),  # Home page
     path('signup/', signup_view, name='sign_up'),
     path('register/', register_user, name='register_user'),
     path('signin/', signin_view, name='sign_in'),
-    path('users/', userslist_view, name="users_list")
+    path('users/', userslist_view, name="users_list"),
+    path(
+        'registration/',
+        registration_completed_view,
+        name="registration_completed"
+    )
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
