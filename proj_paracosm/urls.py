a2680d649b8d40dc9bf2d7e6ea37d9a55899e791
Valner Wasilewski <valnerwasilewski@gmail.com>
2025-06-25 20:55:20 -0300

    :package: Sorted imports

--- proj_paracosm/urls.py
+++ proj_paracosm/urls.py
@@ -20,19 +20,21 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from app_signup.views import (home, register_user, registration_completed_view,
-                              signin_view, signup_view, userslist_view)
+from app_signup.views import (home, home2, register_user,
+                              registration_completed_view, signin_view,
+                              signup_view, userslist_view)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),  # Home page
+    path('home2/', home2, name='home2'),
     path('signup/', signup_view, name='sign_up'),
     path('register/', register_user, name='register_user'),
     path('signin/', signin_view, name='sign_in'),
-    path('users/', userslist_view, name="users_list"),
+    path('users/', userslist_view, name='users_list'),
     path(
         'registration/',
         registration_completed_view,
-        name="registration_completed"
-    )
+        name='registration_completed',
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
