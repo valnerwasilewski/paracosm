2ed29b2f68ba384a47e7af33ad51f13a06c4a859
Valner Wasilewski <valnerwasilewski@gmail.com>
2025-06-25 20:24:17 -0300

    :package: Sorted imports

--- app_signup/admin.py
+++ app_signup/admin.py
@@ -1,3 +1,17 @@
+# Register your models here.
 from django.contrib import admin
 
-# Register your models here.
+from .models import User  # Importe seu modelo User
+
+
+# Registre o modelo User no painel administrativo
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_active', 'created_at')
+    search_fields = ('username', 'email')
+    list_filter = ('is_active', 'created_at')
+
+    # Para adicionar mais funcionalidades personalizadas
+    # def save_model(self, request, obj, form, change):
+    #    # Lógica personalizada ao salvar um modelo
+    #    super().save_model(request, obj, form, change)
